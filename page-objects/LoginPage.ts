b07c07ec293b1a8da3aeccbac5bc2549c84a66bb
pgolyasov <pavel.goliasov@gmail.com>
2025-08-11 14:50:43 +0200

    Visual tests added

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -6,6 +6,7 @@ export class LoginPage extends AbstractPage {
     readonly passwordInput: Locator
     readonly submitButton: Locator
     readonly errorMessage: Locator
+    readonly loginForm: Locator
 
     constructor(page: Page) {
         super(page)
@@ -13,6 +14,7 @@ export class LoginPage extends AbstractPage {
         this.passwordInput = page.locator('#user_password')
         this.submitButton = page.locator('text=Sign in')
         this.errorMessage = page.locator('.alert-error')
+        this.loginForm = page.locator('#login_form')
     }
 
     async login(username: string, password: string) {
@@ -23,5 +25,15 @@ export class LoginPage extends AbstractPage {
 
     async assertErrorMessage() {
         await expect(this.errorMessage).toContainText('Login and/or password are wrong')
+    }
+
+    async snapshotLoginForm() {
+        const screenshot = await this.loginForm.screenshot();
+        await expect(screenshot).toMatchSnapshot('login-form.png');
+    }
+
+    async snapshotErrorMessage() {
+        const screenshot = await this.errorMessage.screenshot();
+        await expect(screenshot).toMatchSnapshot('error-message.png');
     }
 }
